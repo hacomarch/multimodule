647eea6f2cc464ccc30f51f3af59bd0c4c124d48
윤하은 <haco_march@naver.com>
2023-09-26 21:39:38 +0900

    프로파일은 무엇인가?

--- module-api/src/main/java/dev/be/moduleapi/service/DemoService.java
+++ module-api/src/main/java/dev/be/moduleapi/service/DemoService.java
@@ -6,16 +6,28 @@ import dev.be.modulecommon.enums.CodeEnum;
 import dev.be.modulecommon.repositories.MemberRepository;
 import dev.be.modulecommon.service.CommonDemoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class DemoService {
 
-    private final CommonDemoService commonDemoService;
+    @Value("${profile-name}")
+    private String name;
+
     private final MemberRepository memberRepository;
 
     public String save() {
+        System.out.println("name : " + name);
+
+        //프로파일별로 분기를 한다.
+        if (name == "local") {
+            // aaaa
+        } else if (name == "beta") {
+            //bbbb
+        }
+
         memberRepository.save(Member.builder()
                 .name(Thread.currentThread().getName())
                 .build());
